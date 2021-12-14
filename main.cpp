f55ea25c9efa0ac80ffdd80f271e345f35261a19
Jason Montanez <93954157+montanezj@users.noreply.github.com>
2021-12-14 07:35:22 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ const string studentData[] =
 "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-"A5,Jason,Montanez,jmon306@wgu.edu,24,20,30,30,SOFTWARE" };
+"A5,Jason,Montanez,REDACTED,24,20,30,30,SOFTWARE" };
  
 
 
