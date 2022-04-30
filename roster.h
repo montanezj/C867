013f0d996078960bc166cabbb96a99b4771170e7
Jason Montanez <93954157+montanezj@users.noreply.github.com>
2022-04-30 13:16:57 -0700

    Update roster.h

--- roster.h
+++ roster.h
@@ -1,53 +1,33 @@
 #pragma once
+#include <iostream>
+#include <string>
 #include "student.h"
-/*class Repo
-{
-private:
-	int lastIndex = -1;
-	const static int numBooks = 5;
-	Book* bookRepoArray[numBooks];/*NOT declared dynamicallly*/
-
-/*public:
-	/*Note that Repo has no constructor; or DOES IT...?*/
-	/*The parse method goes through a set of comma delimited strings and extracts the data of a book*/
-	//void parse(string row);
-	/*All book data is then passed into add method which creates the Book object and places it in the */
-	//void add(string bID,
-		//string bTitle,
-		//string bAuthor,
-		//double bprice1,
-		//double bprice2,
-		//double bprice3,
-		//BookType bt);
-	//void printAll(); /*Just calls the print() method in Book class for each book*/
-	//void printByBookType(BookType bt);/*Booktype is passed in*/
-	//void printInvalidIDs();
-	//void printAveragePrices();/*Prints the average price for each book*/
-	//void removeBookByID(string bookID);/*Find the book with supplied ID and remove*/
-	//~Repo();
-//};
-
+using namespace std;
 
 class Roster
 {
-private:
-	int lastIndex = -1;
-	const static int numStudent = 5;
-	Student* classRosterArray[numStudent];
-
 public:
-	void parse(string row);
-	void add(string sID,
-		string fName,
-		string lName,
+	const static int numStudents = 5;
+	Student* classRosterArray[numStudents]; //array that holds data from STUDENTS
+	void parse(string studentData);
+	void add(string studentID,
+		string firstName,
+		string lastName,
 		string email,
 		int age,
-		int numberOfDaysToComplete3Courses,
-		DegreeProgram dp);
+		int numberOfDaysToComplete3Courses1,
+		int numberOfDaysToComplete3Courses2,
+		int numberOfDaysToComplete3Courses3,
+		DegreeProgram degreeProgram);
 	void printAll();
-	void printByDegreeProgram(DegreeProgram dp);
-	void printInvalidIDs();
-	void printAge();
+	void printByDegreeProgram(DegreeProgram degreeProgram);
+	void printInvalidEmails();
+	void printAverageDaysInCourse(string studentID);
 	void removeStudentByID(string studentID);
-	~Repo();
+	~Roster(); //deconstructor
+
+private:
+	int lastIndex = -1;
+	int i = 0;
 };
+
