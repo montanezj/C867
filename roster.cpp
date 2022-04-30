d7909359dd5ad47d47076c2c6c9084f52006ae44
Jason Montanez <93954157+montanezj@users.noreply.github.com>
2022-04-30 13:16:32 -0700

    Update roster.cpp

--- roster.cpp
+++ roster.cpp
@@ -1,154 +1,139 @@
+#pragma once
+#include <iostream>
+#include <string>
 #include "roster.h"
-//void Repo::parse(string bookdata)/*this parses out each row - one at a time*/
-//{
-	//BookType bt = UNDECIDED;/*Default Value*/
-	//if (bookdata.at(0) == 'F') bt = FICTION;/*The first character tells us the book type*/
-	//else if (bookdata.at(0) == 'N') bt = NONFICTION;
-
-	//int rhs = bookdata.find(",");/*find the comma*/
-	//string bID = bookdata.substr(0, rhs);/*extract the substring in front of the comma*/
-
-	//int lhs = rhs + 1;/*move past the previous comma*/
-	//rhs = bookdata.find(",", lhs);
-//	string ttl = bookdata.substr(lhs, rhs - lhs);/*book title*/
-
-	//lhs = rhs + 1;/*keep going*/
-	//rhs = bookdata.find(",", lhs);
-	//string aut = bookdata.substr(lhs, rhs - lhs);/*book author*/
-
-	//lhs = rhs + 1; /*keep going*/
-	//rhs = bookdata.find(",", lhs);
-	//double p1 = stod(bookdata.substr(lhs, rhs - lhs));/*first price - convert to double*/
-	//lhs = rhs + 1;
-	//rhs = bookdata.find(",", lhs);
-	//double p2 = stod(bookdata.substr(lhs, rhs - lhs));/*second price*/
-	//lhs = rhs + 1;
-	//rhs = bookdata.find(",", lhs);
-	//double p3 = stod(bookdata.substr(lhs, rhs - lhs));/*third price*/
-	/*Already determined the book type - it was the first thing we did - call add() method*/
-
-	//add(bID, ttl, aut, p1, p2, p3, bt);/*Prices go in SEPARATELY*/
-//}
-	/*The infamous, redundant, and wholly unecessary add method - it only does one thing*/
-//void Repo::add(string bookID, string title, string author, double price1, double price2, double price3, BookType bt)
-//{
-	/*Put prices back into an array for constructor - OK, it does TWO things!*/
-	//double parr[3] = { price1,price2,price3 };
-	/*Make the new Book object. Remember that lastIndex starts at -1 and NOT 0*/
-	///*Remember to use the PREFIX form of ++. Using postfix will MESS YOU UP!*/
-	//bookRepoArray[++lastIndex] = new Book(bookID, title, author, parr, bt);/*Use full constructor*/
-//}
-
-/*display all books using tab-separated output*/
-//void Repo::printAll()
-//{
-
-	//Book::printHeader();/*quick way to do this part*/
-	//for (int i = 0; i <= Repo::lastIndex; i++)
-	//{
-		//cout << bookRepoArray[i]->getID(); cout << '\t';
-		//cout << bookRepoArray[i]->getTitle(); cout << '\t';
-		//cout << bookRepoArray[i]->getAuthor(); cout << '\t';
-		//cout << bookRepoArray[i]->getPrices()[0]; cout << '\t';
-		//cout << bookRepoArray[i]->getPrices()[1]; cout << '\t';
-		//cout << bookRepoArray[i]->getPrices()[2]; cout << '\t';
-		//cout << bookTypeStrings[bookRepoArray[i]->getBookType()] << std::endl;
-	//}
-//}
-
-/*displays only books which match given book type*/
-//void Repo::printByBookType(BookType bt)
-//
-	//Book::printHeader();
-	//for (int i = 0; i <= Repo::lastIndex; i++) {
-	//	if (Repo::bookRepoArray[i]->getBookType() == bt) bookRepoArray[i]->print();
-	//}
-//	cout << std::endl;
-//}
-
-/*Each book's ID must contain an underscore and either an upper case 'X' or a lower case 'x'*/
-//void Repo::printInvalidIDs()
-//{
-	//bool any = false;
-	//for (int i = 0; i <= Repo::lastIndex; i++)
-	//{
-		//string bID = (bookRepoArray[i]->getID());
-	//	if (bID.find('_') == string::npos || (bID.find('X') == string::npos && bID.find('x') == string::npos))
-		//{
-		//	any = true;
-		//	cout << bID << ": " << bookRepoArray[i]->getTitle() << std::endl;
-		//}
-	//}
-//if (!any) cout << "NONE" << std::endl;
-//}
-
-//void Repo::printAveragePrices()
-//{
-	//for (int i = 0; i <= Repo::lastIndex; i++) {
-		//cout << bookRepoArray[i]->getID() << ": ";
-		//cout << (bookRepoArray[i]->getPrices()[0] + bookRepoArray[i]->getPrices()[1] + bookRepoArray[i]->getPrices()[2]) / 3.0 << std::endl;
-	//}
-	//cout << std::endl;
-//}
-
-//void Repo::removeBookByID(string bookID)
-//{
-	//bool success = false;/*assume it's NOT here*/
-	//for (int i = 0; i <= Repo::lastIndex; i++)
-	//{
-	//	if (bookRepoArray[i]->getID() == bookID)
-	//	{
-			//success = true;/*found*/
-			//if (i < numBooks - 1)
-			//{
-			//	Book* temp = bookRepoArray[i];/*swap it with last book*/
-			//	bookRepoArray[i] = bookRepoArray[numBooks - 1];
-			//	bookRepoArray[numBooks - 1] = temp;
-			//}
-			//Repo::lastIndex--;/*LAST BOOK NO LONGER VISIBLE*/
-			/*NOTICE I DID NOT ACTUALLY DELETE THE BOOK! YOU JUST CAN'T SEE IT NOW*/
-		//}
-	//}
-	//if (success)
-	//{
-		//cout << bookID << "removed from repository." << std::endl << std::endl;
-		//this->printAll();/*displays the books 0 the removed one SHOULD NOT DISPLAY*/
-	//}
-	//else cout << bookID << " not found." << std::endl << std::endl;
-//}
-
-//Repo::~Repo()
-//{
-	//cout << "DESTRUCTOR CALLED!!!" << std::endl << std::endl;
-	//for (int i = 0; i < numBooks; i++)
-	//{
-		//cout << "Destroying book #" << i + 1 << std::endl;
-		//delete bookRepoArray[i];
-		//bookRepoArray[i] = nullptr;
-	//}
-//}
-
-
-void Repo::parse(string studentdata)
+using namespace std;
+//creates student object for each student table and it populates the classRosterArray
+void Roster::parse(string studentData)
 {
-	DegreeProgram dp = SECURITY; //this section won't work, because it's reading the 0th index which is a1-a5, in the code ex. it has f for fiction and n for non-fiction
-	if (studentdata.at(0) == 'Network') dp = NETWORK;
-	else if (studentdata.at(0) == 'Software') dp = SOFTWARE;
-
-	int rhs = studentdata.find(",");	/*finds comma in example*/
-	string sID = studentdata.substr(0, rhs);
+	int rhs = studentData.find(",");	/*finds comma in example*/
+	string studentID = studentData.substr(0, rhs);
 
 	int lhs = rhs + 1; /*move past the previous comma in example*/
-	rhs = studentdata.find(",", lhs);
-	string ttl = studentdata.substr(lhs, rhs - lhs);
+	rhs = studentData.find(",", lhs);
+	string firstName = studentData.substr(lhs, rhs - lhs);
 
 	lhs = rhs + 1;
-	rhs = studentdata.find(",", lhs);
-	string ema = studentdata.substr(lhs, rhs - lhs);
+	rhs = studentData.find(",", lhs);
+	string lastName = studentData.substr(lhs, rhs - lhs);
 
 	lhs = rhs + 1;
-	rhs = studentdata.find(".", lhs);
-	int age
+	rhs = studentData.find(".", lhs);
+	string email = studentData.substr(lhs, rhs - lhs);
 
-};
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int age = stoi(studentData.substr(lhs, rhs - lhs));
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int days1 = stoi(studentData.substr(lhs, rhs - lhs));
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int days2 = stoi(studentData.substr(lhs, rhs - lhs));
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int days3 = stoi(studentData.substr(lhs, rhs - lhs));
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	
+	DegreeProgram degreeProgram;
+	string degreeTypeStrings = studentData.substr(lhs, rhs - lhs);
+	if (degreeTypeStrings == "SECURITY") {
+		degreeProgram = DegreeProgram::SECURITY;
+	}
+	else if (degreeTypeStrings == "NETWORK") {
+		degreeProgram = DegreeProgram::NETWORK;
+	}
+	else if (degreeTypeStrings == "SOFTWARE") {
+		degreeProgram = DegreeProgram::SOFTWARE;
+	}
+
+	add(studentID, firstName, lastName, email, age, days1, days2, days3, degreeProgram);
+}
+
+void Roster::add(string studentID, string firstName, string lastName, string email, int age, int days1, int days2, int days3, DegreeProgram degreeProgram)
+{
+	int darr[3]{ days1, days2, days3 }; //constructor - puts days into an array
+	classRosterArray[++lastIndex] = new Student(studentID, firstName,lastName, email, age, darr, degreeProgram);
+}
+//removes students from roster by student ID, error message printed if student does not exist
+void Roster::removeStudentByID(string studentID) {
+	bool found = false;
+	for (i = 0; i <= Roster::lastIndex; i++) {
+		if (classRosterArray[i]->getStudentID() == studentID) {
+			found = true;
+			if (i < numStudents - 1) {
+				Student* temp = classRosterArray[i];
+				classRosterArray[i] = classRosterArray[numStudents - 1];
+				classRosterArray[numStudents - 1] = temp;
+			}
+			Roster::lastIndex--;
+
+			//swaps new last index to maintain proper formatting (A1, A2, A4, A5)
+			Student* temp = classRosterArray[Roster::lastIndex];
+			classRosterArray[Roster::lastIndex] = classRosterArray[Roster::lastIndex - 1];
+			classRosterArray[Roster::lastIndex - 1] = temp;
+		}
+	}
+	if (found) {
+		cout << "Student " << studentID << " has been removed" << endl;
+	}
+	else {
+		cout << "Student " << studentID << " was not found" << endl;
+	}
+}
+//print function for all students in classRosterArray
+void Roster::printAll()
+{
+	cout << "List of Student Data" << endl;
+	for (int i = 0; i <= Roster::lastIndex; i++)
+	{
+		classRosterArray[i]->print();
+	}
+}
+
+//prints the average amount of days the student takes to complete the 3 courses
+void Roster::printAverageDaysInCourse(string studentID) {
+	for (i = 0; i <= Roster::lastIndex; i++) {
+		if (classRosterArray[i]->getStudentID() == studentID) {
+			cout << studentID << " | ";
+			cout << ((classRosterArray[i]->getDaysToComplete3Courses()[0] + classRosterArray[i]->getDaysToComplete3Courses()[1] + classRosterArray[i]->getDaysToComplete3Courses()[2]) / 3);
+			cout << endl;
+		}
+
+	}
+}
+
+//checks to see if email is valid
+//valid emails should follow the format of abc@fakemail.com
+void Roster::printInvalidEmails() {
+	bool invalid = false;
+	for (i = 0; i <= Roster::lastIndex; i++) {
+		string email = (classRosterArray[i]->getEmail());
+		string studentID = (classRosterArray[i]->getStudentID());
+		if (email.find('@') == string::npos || email.find('.') == string::npos || email.find(' ') != string::npos) {
+			invalid = true;
+			cout << studentID << " | " << email << endl;
+		}
+		else if (!invalid) {
+			cout << "No Invalid Email Addresses" << endl;
+		}
+	}
+}
+
+//prints student info under the specified degreeprogram
+void Roster::printByDegreeProgram(DegreeProgram degreeProgram)
+{
+	for (int i = 0; i <= Roster::lastIndex; i++) {
+	if (Roster::classRosterArray[i]->getDegreeProgram() == degreeProgram) classRosterArray[i]->print();
+	}
+}
+
+//deconstructor
+Roster::~Roster() {
+	for (i = 0; i < numStudents; ++i) {
+		delete classRosterArray[i];
+		classRosterArray[i] = nullptr;
+	}
+}
 
