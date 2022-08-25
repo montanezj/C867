ff5082a7cc5ad170c18f9b50cab502079fb59c16
Jason Montanez <93954157+montanezj@users.noreply.github.com>
2022-08-24 18:20:02 -0700

    Update degree.h

--- Project1/degree.h
+++ Project1/degree.h
@@ -1,12 +1,8 @@
-#pragma once
+#pragma once	
 #include <string>
-
-//enum BookType { UNDECIDED, FICTION, NONFICTION };
-
-/*Parallel array pops out a string for each book type - use the book type as an index*/	
-//static const std::string bookTypeStrings[] = { "UNDECIDED", "FICTION", "NONFICTION" };
-
-
-enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
-
-static const std::string degreeTypeStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+#include <iostream>
+#include <iomanip>
+using namespace std;
+//enumerates through data type DegreeProgram for data type values Security, Network, and Software
+enum DegreeProgram { NETWORK, SECURITY, SOFTWARE };
+static const string degreeTypeStrings[] = { "NETWORK", "SECURITY", "SOFTWARE" };
