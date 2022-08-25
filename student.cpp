a5f9e0c42d6072f08c106a4a88fca6c420c7af43
Jason Montanez <93954157+montanezj@users.noreply.github.com>
2022-08-24 18:22:57 -0700

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,63 +1,70 @@
-#pragma once
-#include <iostream>
-#include <string>
 #include "student.h"
-#include "degree.h"
-using namespace std;
 
-Student::Student()
-{
-	this->studentID = "";
-	this->firstName = "";
-	this->lastName = "";
-	this->email = "";
-	this->age = 0;
-	for (int i = 0; i < days; i++) { this->numberOfDaysToComplete3Courses[i] = 0; }
-	this->degreeProgram = DegreeProgram::SECURITY;
+Student::Student() {
+
+	this->studID = "";
+	this->fName = "";
+	this->lName = "";
+	this->studEmail = " ";
+	this->studAge = 0;
+	for (int i = 0; i < days; i++)
+		this->numberOfDaysToComplete3Courses[i] = 0;
+	this->degreeprogram = DegreeProgram::NETWORK;
+
 }
 
-Student::Student(string studentID, string firstName, string lastName, string email, int age, int numberOfDaysToComplete3Courses[], DegreeProgram degreeProgram)
-{
-	this->studentID = studentID;
-	this->firstName = firstName;
-	this->lastName = lastName;
-	this->email = email;
-	this->age = age;
-	for (int i = 0; i < days; i++) { this->numberOfDaysToComplete3Courses[i] = numberOfDaysToComplete3Courses[i]; }
-	this->degreeProgram = degreeProgram;
+Student::Student(string studID, string fname, string lname, string email, int age,
+	int numberOfDaysToComplete3Courses[], DegreeProgram degreeprog) {
+
+	this->studID = studID;
+	this->fName = fname;
+	this->lName = lname;
+	this->studEmail = email;
+	this->studAge = age;
+
+	for (int i = 0; i < days; i++)
+		this->numberOfDaysToComplete3Courses[i] = numberOfDaysToComplete3Courses[i];
+	this->degreeprogram = degreeprog;
+
+
 }
 
-Student::~Student() {}//accessor (getter)
+Student::~Student() {}
 
-string Student::getStudentID() { return studentID; }
-string Student::getFirstName() { return firstName; }
-string Student::getLastName() { return lastName; }
-string Student::getEmail() { return firstName; }
-int Student::getAge() { return age; }
-int* Student::getDaysToComplete3Courses() { return numberOfDaysToComplete3Courses; }
-DegreeProgram Student::getDegreeProgram() { return degreeProgram;  }
 
-//mutator(setter)
-void Student::setStudentID(string ID) { this->studentID = ID; }
-void Student::setFirstName(string firstName) { this->firstName = firstName;  }
-void Student::setLastName(string lastName) { this->lastName = lastName; }
-void Student::setEmail(string email) { this->email = email; }
-void Student::setAge(int ageToSet) { this->age = ageToSet; }
-void Student::setDaysToComplete3Courses(int daysToSet[]) {
-	for (int i = 0; i < days; i++) this->numberOfDaysToComplete3Courses[i] = daysToSet[i];
+string Student::getStudentID() { return this->studID; }
+string Student::getFirstName() { return this->fName; }
+string Student::getLastName() { return this->lName; }
+string Student::getEmail() { return this->studEmail; }
+int Student::getAge() { return this->studAge; }
+int* Student::getDaysToComplete3Courses() { return this->numberOfDaysToComplete3Courses; }
+DegreeProgram  Student::getDegreeProgram() { return this->degreeprogram; }
+
+
+void  Student::setStudentID(string studID) { this->studID = studID; }
+void  Student::setFirstName(string fname) { this->fName = fname; }
+void  Student::setLastName(string lname) { this->lName = lname; }
+void  Student::setAge(int numAge) { this->studAge = numAge; }
+void  Student::setLastNamesetEmail(string email) { this->studEmail = email; }
+void  Student::setDaysToComplete3Courses(int numberOfDaysToComplete3Courses[]) {
+	for (int i = 0; i < days; i++) this->numberOfDaysToComplete3Courses[i] = numberOfDaysToComplete3Courses[i];
+}
+void  Student::setDegreeProgram(DegreeProgram degreeprog) { this->degreeprogram = degreeprog; }
+
+void Student::printHeader() { 
 }
 
-void Student::setDegreeProgram(DegreeProgram degreeToSet) {
-	degreeProgram = degreeToSet;
+
+void Student::print() {
+	cout << this->getStudentID() << '\t';
+	cout << this->getFirstName() << '\t';
+	cout << this->getLastName() << '\t';
+	cout << this->getAge() << '\t';
+	cout << this->getEmail() << '\t';
+	cout << this->getDaysToComplete3Courses()[0] << '\t';
+	cout << this->getDaysToComplete3Courses()[1] << '\t';
+	cout << this->getDaysToComplete3Courses()[2] << '\t';
+	cout << this->getDaysToComplete3Courses()[3] << '\t';
+	cout << degreeTypeStrings[this->getDegreeProgram()] << '\n'; 
+
 }
-
-//print function
-void Student::print()
-{
-	cout << getStudentID() << "\t" << "First Name: " << getFirstName() << "\t" << "Last Name: " << getLastName() << "\t" << "Age: " << getAge() << "\t";
-	cout << "daysInCourse:  {" << getDaysToComplete3Courses()[0] << ", " << getDaysToComplete3Courses()[1] << ", " << getDaysToComplete3Courses()[2] << "} " << "Degree Program: ";
-	cout << degreeTypeStrings[(int)getDegreeProgram()] << endl;
-};
-
-
-
