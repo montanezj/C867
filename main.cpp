f36608ff1a63ef3ec100d94a2c9da75fed64eb63
Jason Montanez <93954157+montanezj@users.noreply.github.com>
2022-08-24 18:21:27 -0700

    Update main.cpp
    Final update to project

--- main.cpp
+++ main.cpp
@@ -1,62 +1,64 @@
-#pragma once
-#include <iostream>
-#include <string>
 #include "roster.h"
-using namespace std;
 
-int main() {
-	//prints out my info, 
+
+
+
+
+int main() { //OBJECTIVE F. create a main function.
+	const string studentData[] =
+
+	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+		"A5,Jason,Montanez,jmon306@wgu.edu,24,30,30,30,SOFTWARE" 
+
+
+	};
+
+	const int numStudents = 5;
+	Roster classRoster;
+
+
+	
 	cout << "C867-Scripting & Programming:Applications" << endl;
 	cout << "Language: C++" << endl;
 	cout << "Student ID: 001389569" << endl;
 	cout << "Name: Jason Montanez" << endl;
 	cout << endl;
 
-	const int numStudents = 5;
-
-	//student info
-	const string studentData[] =
-	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-	"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-	"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-	"A5,Jason,Montanez,jmon306@wgu.edu,24,20,30,30,SOFTWARE" };
-
-	Roster classRoster;
-
-	//adds students to roster
-	for (int i = 0; i < numStudents; i++) {
-		classRoster.parse(studentData[i]);
-	}
-
+	for (int i = 0; i < numStudents; i++)
+		classRoster.parseData(studentData[i]);
+	cout << "Displaying all students:  " << endl;
 	classRoster.printAll();
+	cout << endl << endl;
+
+	cout << "Displaying invalid emails: " << endl;
 	cout << endl;
 
-	cout << "Invalid Email Addresses" << endl;
 	classRoster.printInvalidEmails();
 	cout << endl;
 
-	cout << "Average Days In Course" << endl;
-	for (int i = 0; i < numStudents; i++) {
-		classRoster.printAverageDaysInCourse(classRoster.classRosterArray[i]->getStudentID());
-	}
+
+	classRoster.printAverageDaysInCourse("A1");
+	classRoster.printAverageDaysInCourse("A2");
+	classRoster.printAverageDaysInCourse("A3");
+	classRoster.printAverageDaysInCourse("A4");
+	classRoster.printAverageDaysInCourse("A5");
 	cout << endl;
 
-	cout << "List of Software Students" << endl;
-	classRoster.printByDegreeProgram(DegreeProgram::SOFTWARE);
-	cout << endl;
+	cout << "Showing students in degree program: SOFTWARE" << endl;
+	classRoster.printByDegreeProgram(SOFTWARE);
 
-	classRoster.removeStudentByID("A3");
-	cout << endl;
+	classRoster.remove("A3"); 
 
 	classRoster.printAll();
-	cout << endl;
 
-	classRoster.removeStudentByID("A3");
-	cout << endl;
+	classRoster.remove("A3");
+
+
+	system("pause");
 
 	return 0;
 
-};
+}
 
 
