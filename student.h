374891e2d7610c9b477e14ef14bc3d2b7d73a491
Jason Montanez <93954157+montanezj@users.noreply.github.com>
2022-08-24 18:23:20 -0700

    Update student.h

--- student.h
+++ student.h
@@ -1,43 +1,61 @@
 #pragma once
-#include <iostream>
-#include <string>
 #include "degree.h"
 using namespace std;
 
-class Student
-{   
+
+class Student {
+
+
 public:
-    Student();
-    Student(string studentID, string firstName, string lastName, string email, int age, int numberOfDaysToComplete3Courses[], DegreeProgram degreeProgram);
-    ~Student();
 
-    string getStudentID();
-    string getFirstName();
-    string getLastName();
-    string getEmail();
-    int getAge();
-    int* getDaysToComplete3Courses();
-    DegreeProgram getDegreeProgram();
 
-    void setStudentID(string ID);
-    void setFirstName(string firstName);
-    void setLastName(string lastName);
-    void setEmail(string email);
-    void setAge(int ageToSet);
-    void setDaysToComplete3Courses(int daysToSet[]);
-    void setDegreeProgram(DegreeProgram degreeProgram);
+	Student();
+	Student(string studID,
+		string fName,
+		string lName,
+		string studEmail,
+		int studAge,
+		int numberOfDaysToComplete3Courses[],
+		DegreeProgram degreeProgram);
+	~Student();
 
-    void print();
 
-    const static int days = 3;
+
+
+	string getStudentID();
+	string getFirstName();
+	string getLastName();
+	string getEmail();
+	int getAge();
+	int* getDaysToComplete3Courses();
+	DegreeProgram getDegreeProgram();
+
+
+
+	void setStudentID(string studID);
+	void setFirstName(string fName);
+	void setLastName(string lName);
+	void setLastNamesetEmail(string studEmail);
+	void setAge(int numAge);
+	void setDaysToComplete3Courses(int numberOfDaysToComplete3Courses[]);
+	void setDegreeProgram(DegreeProgram degreeProgram);
+
+
+	static void printHeader();
+	void print();
+
+	const static int days = 3;
 
 private:
-    string studentID;
-    string firstName;
-    string lastName;
-    string email;
-    int age;
-    int numberOfDaysToComplete3Courses[days];
-    DegreeProgram degreeProgram;
-    int i = 0;
-};
+
+
+	string studID;
+	string fName;
+	string lName;
+	string studEmail;
+	int studAge;
+	int numberOfDaysToComplete3Courses[days];
+	DegreeProgram degreeprogram;
+
+
+}; 
